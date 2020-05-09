03f39d5e133b3cf72ab6eed08c333fc3b8793fb7
Stuart Langridge <sil@kryogenix.org>
2020-05-09 10:20:28 +0100

    Ubuntu handle dupes, allow calling direct from command line for ease of testing

--- projects/ubuntu_project.py
+++ projects/ubuntu_project.py
@@ -2,13 +2,18 @@ import random
 import urllib.request
 from bs4 import BeautifulSoup
 import math
+import sys
 
 
-def get(max_bug):
+def get(max_bug, override_result=None):
     # get a random int up to max_bug, but biased towards the more recent
     # https://gamedev.stackexchange.com/a/116875 with min = 0
     while True:
-        result = math.floor(max_bug * pow(random.random(), 0.8))
+        if override_result:
+            result = override_result
+            override_result = None
+        else:
+            result = math.floor(max_bug * pow(random.random(), 0.8))
         bug_page = "https://pad.lv/{}".format(result)
         print("trying", bug_page)
         try:
@@ -17,8 +22,13 @@ def get(max_bug):
             continue
         soup = BeautifulSoup(fp.read(), "lxml")
         fp.close()
+        dupe = soup.select("div#bug-is-duplicate .bug-duplicate-details a")
+        if dupe:
+            override_result = dupe[0]["href"].split("/")[-1]
+            print("Duplicate of", override_result)
+            continue
         status = soup.select("div.status-content")[0].text.strip()
-        if status in ("New", "Incomplete"):
+        if status in ("New", "Incomplete", "Confirmed"):
             title = soup.select("h1#edit-title")[0].text.strip()
             return {
                 "title": title,
@@ -27,7 +37,6 @@ def get(max_bug):
         else:
             print("bad status", status)
             continue
-    return random.randint(1, max_bug)
 
 
 def get_max_bug_number():
@@ -35,3 +44,10 @@ def get_max_bug_number():
         "https://bugs.launchpad.net/ubuntu/+bugs?orderby=-id&start=0")
     soup = BeautifulSoup(fp.read(), "lxml")
     return int(soup.select("span.bugnumber")[0].text[1:])
+
+
+if __name__ == "__main__":
+    # for testing, run as python3 projects/ubuntu_project.py
+    # or python3 projects/ubuntu_project.py 434733
+    max_bug = get_max_bug_number()
+    print(get(max_bug, sys.argv[1] if len(sys.argv) > 1 else None))
